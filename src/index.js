214c9c368d0672f6528a7bff27fe6e49a5b6561e
HalimHamidov <halimjonikgmail.com>
2020-03-21 11:20:38 +0300

    docs: solved and push of brackets.js

--- src/index.js
+++ src/index.js
@@ -1,56 +1,24 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-  var stack = [];
+  module.exports = function check(str, bracketsConfig) {
+    let j = 0;
+    let len = bracketsConfig.length;
+    let temp = "";
+    let brackets = "";
+    while (j != len) {
+        j = 0;
+        let i = 0;
+        while (i < len) {
+            brackets = bracketsConfig[i][0] + bracketsConfig[i][1];
+            temp = str.replace(brackets, "");
+            if (str === temp) {
+                j++;
+            }
+            str = temp;
+            i++;
+        }
+    }
+    return str.length === 0;
+}
 
-  var lenstr = str.length;
-  if (lenstr % 2 != 0) return false;
-  
-  var lenConfig = bracketsConfig.length;
-  var checkeven = 1;
-  var cheven;
-  var chClose;
-  for (var i = 0; i < lenstr; i++) {
-      var ch = str.charAt(i);
-      var close = false;
-      var open = false;
-      var even = false;
-      for (let j = 0; j < lenConfig; j++) {
-          if (bracketsConfig[j][0] == bracketsConfig[j][1] && ch == bracketsConfig[j][0]) {
-              even = true;
-              checkeven = (-1) * checkeven;
-              cheven = ch;
-              console.log('checkeven = ', checkeven)
-              break;
-          }
-          if (ch == bracketsConfig[j][0]) {
-              open = true;
-              break;
-          }
-          if (ch == bracketsConfig[j][1]) {
-              close = true;
-              chClose = bracketsConfig[j][0];
-              break;
-          }
-      }
-      if (even == true) {
-          if (checkeven == -1) {
-              stack.push(ch);
-              console.log('push=', ch);
-          }
-          if (checkeven == 1 && cheven == stack[stack.length - 1]) {
-              console.log(' pop stack[stack.length-1]=', stack[stack.length - 1], ', ch= ', ch)
-              stack.pop();
-          }
-      } else {
-          if (open === true) {
-              stack.push(ch);
-              console.log('push=', ch);
-          }
-          if (close === true && chClose == stack[stack.length - 1]) {
-              console.log(' pop stack[stack.length-1]=', stack[stack.length - 1], ', ch= ', ch)
-              stack.pop();
-          }
-      }
-  }
-  return (stack.length === 0);
-};
+// первым параметром передается строка, 
+// вторым - массив массивов из скобок, которые встречаются в строке.
+// заменяет в строке str все подстроки brackets на пустую строку
